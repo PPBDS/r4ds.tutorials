4d71a293543e1936a3559de37afd1b3d50b52295
David Kane <dave.kane@gmail.com>
2023-08-28 15:16:12 -0400

    skip knit test on CRAN

--- tests/testthat/test-tutorials.R
+++ tests/testthat/test-tutorials.R
@@ -4,10 +4,16 @@
 # all the paths from this version of the package. Instead, it is getting the
 # paths from the most recently installed version of the package. So, you really
 # need to install before testing. If true, that is a hack!
+
+# Turns out that CRAN does not like tests to take longer than 10 minutes. Alas,
+# we now have so many tests, that the total time is 15 minutes, at least on
+# Windows. So, we skip knitting all the tutorials on CRAN. (We can keep the
+# expected components check because those are very fast.)
   
 tut_paths <- tutorial.helpers::return_tutorial_paths("r4ds.tutorials")
 
 test_that("All tutorials can be knit without error", {
+  skip_on_cran()
   expect_null(
     tutorial.helpers::knit_tutorials(tut_paths)
   )
