9690a530c3a1a35bf6d523a6e4dc83162bc08b9e
David Kane <dave.kane@gmail.com>
2024-08-19 10:18:08 -0400

    post meeting

---
+++ inst/tutorials/24-web-scraping/data/imdb_files/01EjywnajPL.js
@@ -0,0 +1,46 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+(window.webpackJsonpBoomer=window.webpackJsonpBoomer||[]).push([["commons.classnames"],{Kkip5aHMh7:function(r,n,o){var e;
+/*!
+  Copyright (c) 2017 Jed Watson.
+  Licensed under the MIT License (MIT), see
+  http://jedwatson.github.io/classnames
+*/
+/*!
+  Copyright (c) 2017 Jed Watson.
+  Licensed under the MIT License (MIT), see
+  http://jedwatson.github.io/classnames
+*/
+!function(){"use strict";var o={}.hasOwnProperty;function s(){for(var r=[],n=0;n<arguments.length;n++){var e=arguments[n];if(e){var a=typeof e;if("string"===a||"number"===a)r.push(e);else if(Array.isArray(e)&&e.length){var p=s.apply(null,e);p&&r.push(p)}else if("object"===a)for(var t in e)o.call(e,t)&&e[t]&&r.push(t)}}return r.join(" ")}r.exports?(s.default=s,r.exports=s):void 0===(e=function(){return s}.apply(n,[]))||(r.exports=e)}()}}]);
+
+}
+/*
+     FILE ARCHIVED ON 01:10:15 Feb 01, 2022 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 14:09:17 Aug 19, 2024.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 0.802
+  exclusion.robots: 0.031
+  exclusion.robots.policy: 0.015
+  esindex: 0.019
+  cdx.remote: 27.541
+  LoadShardBlock: 256.266 (6)
+  PetaboxLoader3.datanode: 130.483 (7)
+  load_resource: 58.131
+  PetaboxLoader3.resolve: 25.557
+*/
---
+++ inst/tutorials/24-web-scraping/data/imdb_files/01eEXY1YetL.js
@@ -0,0 +1,35 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+(window.webpackJsonpBoomer=window.webpackJsonpBoomer||[]).push([["commons.detect-node"],{s1DKtra4BZ:function(o,e){o.exports=!1}}]);
+
+}
+/*
+     FILE ARCHIVED ON 01:10:16 Feb 01, 2022 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 14:09:20 Aug 19, 2024.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 0.582
+  exclusion.robots: 0.02
+  exclusion.robots.policy: 0.009
+  esindex: 0.01
+  cdx.remote: 6.176
+  LoadShardBlock: 105.502 (6)
+  PetaboxLoader3.datanode: 138.03 (7)
+  load_resource: 763.218
+  PetaboxLoader3.resolve: 725.379
+*/
